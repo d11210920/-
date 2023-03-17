e5b4289b97918521e0ee001d590e2a59a1d03a3a
d11210920 <d11210920@naver.com>
2023-03-17 23:27:03 +0900

    1238edit

--- 알고리즘/1238/1238/소스.cpp
+++ 알고리즘/1238/1238/소스.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int n, m, x;
 vector<pair<int,int>> road[1001];
 int dp[1001];
-bool visited[1001];
+int tmp[1001];
 
 void input() {
 	cin >> n >> m >> x;
@@ -21,27 +21,27 @@ void input() {
 }
 
 void reset() {
-	for (int i = 0; i <= n; i++) {
+	for (int i = 1; i <= n; i++) {
 		dp[i] = MAX;
-		visited[i] = false;
 	}
 }
 
 void dijkstra(int idx) {
-	priority_queue<pair<int, pair<int, int>>> pq;
-	pq.push({ -road[idx].front().second, {idx,road[idx].front().first } });
+	priority_queue<pair<int,int>> pq;
+	pq.push({ 0, idx });
 	dp[idx] = 0;
 	while (!pq.empty()) {
 		int cost = -pq.top().first;
-		int start = pq.top().second.first;
-		int arrive = pq.top().second.second;
+		int cur = pq.top().second;
 		pq.pop();
 
-		for (int i = 0; i < road[start].size(); i++) {
-			if (dp[arrive] > cost + road[start][i].second) {
-				dp[arrive] = cost + road[start][i].second;
-				if (arrive == x)return;
-				pq.push({ -dp[arrive],{start,road[start][i].first} });
+		for (int i = 0; i < road[cur].size(); i++) {
+			int next = road[cur][i].first;
+			int newCost = cost + road[cur][i].second;
+
+			if (dp[next] > newCost) {
+				dp[next] = newCost;
+				pq.push({ -dp[next],next });
 			}
 		}
 	}
@@ -50,18 +50,16 @@ void dijkstra(int idx) {
 
 void solve() {
 	input();
-	int max = 0;
-	int max_idx = 0;
-	reset();
 	for (int i = 1; i <= n; i++) {
-		dijkstra(i);
-		if (max < dp[x]) {
-			max = dp[x];
-			max_idx = i;
-		}
 		reset();
+		dijkstra(i);
+		tmp[i] = dp[x];
 	}
-	cout << max;
+	reset();
+	dijkstra(x);
+	for (int i = 1; i <= n; i++)tmp[i] += dp[i];
+	sort(tmp + 1, tmp + n + 1);
+	cout << tmp[n];
 }
 
 int main() {
