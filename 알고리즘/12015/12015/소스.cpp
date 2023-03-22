2d296fb120d5fc9126d9d87c185bb51e453f1d81
d11210920 <d11210920@naver.com>
2023-03-22 18:44:21 +0900

    12015

---
+++ 알고리즘/12015/12015/소스.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int arr[1000001];
+vector<int> dp;
+
+/*
+	ÀÌºÐÅ½»ö!!
+*/
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
+
+int binarySearch(int x) {
+	int st = 0;
+	int ed = dp.size() - 1;
+
+	while (ed > st) {
+		int mid = (st + ed) / 2;
+		if (dp[mid] >= x) {
+			ed = mid;
+		}
+		else st = mid + 1;
+	}
+	return ed;
+}
+
+
+void solution() {
+	dp.push_back(arr[0]);
+	for (int i = 1; i < n; i++) {
+		if (dp.back() < arr[i])dp.push_back(arr[i]);
+		else {
+			int idx = binarySearch(arr[i]);
+			dp[idx] = arr[i];
+		}
+	}
+}
+void solve() {
+	input();
+	solution();
+	cout << dp.size();
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+
+	return 0;
+}
